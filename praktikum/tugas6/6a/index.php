0d6af5d8f92ab6345a1ce32bbec7656d0ace6d86
Raihananr <raihananrr@gmail.com>
2021-05-25 00:04:48 +0700

    benerin typo code css

--- praktikum/tugas6/6a/index.php
+++ praktikum/tugas6/6a/index.php
@@ -8,7 +8,7 @@ NRP : 203040113
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Latihan 5E</title>
+    <title>Latihan 6a</title>
      <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
