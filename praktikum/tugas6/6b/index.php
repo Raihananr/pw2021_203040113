0a44ac8d628d657c3ae0a071200ed920212e3db4
Raihananr <raihananrr@gmail.com>
2021-05-25 01:07:40 +0700

    tugas 6 benerin coding css

--- praktikum/tugas6/6b/index.php
+++ praktikum/tugas6/6b/index.php
@@ -9,11 +9,12 @@ NRP : 203040113
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Latihan 6b</title>
-     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
     <a href="php/login.php">
-            <button type="">Masuk ke halaman admin</button>
+        <button type="">Masuk ke halaman admin</button>
     </a>
 </body>
 </html>
